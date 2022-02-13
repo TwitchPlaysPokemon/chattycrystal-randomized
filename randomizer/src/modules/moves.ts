2580813215ba647b84d0151b814ba6be38c57595
M4 <dave.was.r@gmail.com>
2022-02-13 09:27:36 -0500

    Revamped learnset randomizer

--- randomizer/src/modules/moves.ts
+++ randomizer/src/modules/moves.ts
@@ -1,19 +1,27 @@
 import { randomInt } from "crypto";
+import BaseStats from "../models/base_stats";
 import EvosAttacks from "../models/evos_attacks";
 import Move from "../models/move";
 import MoveConstantsParser from "../parsers/constants/move_constants";
+import PokemonConstantsParser from "../parsers/constants/pokemon_constants";
+import BaseStatsParser from "../parsers/data/base_stats";
 import EvosAttacksParser from "../parsers/data/evos_attacks";
 import MovesParser from "../parsers/data/moves";
+import IncludeListParser from "../parsers/include_list";
 import SplitCollectionParser from "../parsers/splitcollection";
+import { PickCascade, Shuffle } from "../utils/pick";
 
 export default class LearnsetRandomizer implements RandoModule {
     command = "learnsets"
-    helpText = "Randomizes pokemon learnsets. Replaces non-attacks with non-attacks.";
+    helpText = "Randomizes pokemon learnsets. Replaces non-attacks with non-attacks and STAB with STAB.";
     operation() {
         const moveConstants = new MoveConstantsParser("constants/move_constants.asm").data;
 
         const moveLookup: { [key: string]: Move } = {};
         new MovesParser("data/moves/moves.asm").data.forEach((m, i) => moveLookup[moveConstants[i + 1]] = m);
+
+        const pokeConstants = new PokemonConstantsParser("constants/pokemon_constants.asm").data;
+        const monStatsLookup = [...new IncludeListParser("data/pokemon/base_stats_old.asm", BaseStatsParser).data, ...new IncludeListParser("data/pokemon/base_stats_new.asm", BaseStatsParser).data].map(m => m.data);
 
         const evosAttacksParser = new SplitCollectionParser<EvosAttacks, EvosAttacksParser>(["data/pokemon/evos_attacks_kanto.asm", "data/pokemon/evos_attacks_johto.asm", "data/pokemon/evos_attacks_new.asm"], EvosAttacksParser);
 
@@ -23,22 +31,24 @@ export default class LearnsetRandomizer implements RandoModule {
 
         evosAttacks.forEach((mon, i) => {
             let availableMoves = moveConstants.slice(1).filter(m => m != "STRUGGLE" && m != "CHATTY_HP");
-            console.log(mon.label);
-            mon.attacks = mon.attacks.map(atk => {
-                if (atk.move.includes(' '))
-                    atk.move = atk.move.split(' ').shift(); // Parser picked up someone's comment
-                const oldMoveInfo = moveLookup[atk.move];
-                let newMoveIndex: number;;
-                let newMove: string;
-                let newMoveInfo: Move;
-                do {
-                    newMoveIndex = randomInt(0, availableMoves.length);
-                    newMove = availableMoves[newMoveIndex];
-                    newMoveInfo = moveLookup[newMove];
-                } while (newMoveInfo.doesDamage != oldMoveInfo.doesDamage);
+            console.log(pokeConstants[i]);
+            const monStats = monStatsLookup[i];
 
-                availableMoves.splice(newMoveIndex, 1);
-                atk.move = newMove;
+            const attacks = Shuffle(mon.attacks.map(atk => atk.move.split(' ').shift())).map(atk => {
+                const oldMoveInfo = moveLookup[atk];
+                const isStab = (m: Move) => m.doesDamage && (m.type == monStats.type1 || m.type == monStats.type2);
+                const shouldBeStab = isStab(oldMoveInfo);
+
+                const newMove = PickCascade(availableMoves,
+                    m => moveLookup[m].doesDamage == oldMoveInfo.doesDamage, // replace non-attack with non-attack
+                    m => !shouldBeStab || isStab(moveLookup[m]), // replace STAB with STAB
+                    m => m != atk // don't pick same move
+                );
+                return newMove || atk;
+            });
+
+            mon.attacks = mon.attacks.map(atk => {
+                atk.move = attacks.shift() || atk.move;
                 return atk;
             })
 
